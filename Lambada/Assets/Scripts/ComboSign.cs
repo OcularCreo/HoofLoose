e3b84ccce18e7b84bc6b35c1e40072dd17ec6b2e
hujillian <hujillian123@gmail.com>
2025-01-28 02:39:01 -0500

    fixed code + animations: sign seems to work

--- Lambada/Assets/Scripts/ComboSign.cs
+++ Lambada/Assets/Scripts/ComboSign.cs
@@ -9,16 +9,12 @@ public class ComboSign : MonoBehaviour
     [SerializeField] GameManager gameManager;
     [SerializeField] Image signImage;
 
-    //private bool slideOut;
-    private bool comboCashed;
     private int prevCombo;
     private int currentCombo;
 
     // Start is called before the first frame update
     void Start()
     {
-        //slideOut = false;
-        comboCashed = false;
         prevCombo = 0;
         currentCombo = 0;
     }
@@ -26,41 +22,41 @@ public class ComboSign : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if the combo is a multiple of 8, and is not 0, play the animation to make the sign slide in
-        /*        if((gameManager.GetComponent<GameManager>().combo % 8 == 0) && (gameManager.GetComponent<GameManager>().combo != 0))
-                {
-                    signAnimator.SetBool("NewCombo", true);
-                    slideOut = true;
-                }
-                else if ((gameManager.GetComponent<GameManager>().combo % 8 == 1) || (gameManager.GetComponent<GameManager>().combo == 0))
-                {
-                    if (slideOut)
-                    {
-                        signAnimator.SetBool("ComboDone", true);
-                        slideOut = false;
-                    }
-                }
-                // this was such a good idea but it doesn't work because you can bypass 8 in the game
-         */
-
-
-        //
-
+        // get the current combo number
         currentCombo = gameManager.GetComponent<GameManager>().combo;
 
+        // if the player reached a new multiple of 8
         if ((currentCombo / 8) > (prevCombo / 8))
         {
-            signAnimator.SetBool("ComboDone", false);
-            signAnimator.SetBool("NewCombo", true);
+            if(currentCombo >= 16) // if this is not the first combo
+            {
+                // sign slides out then back in
+                signAnimator.SetBool("NewCombo", false);
+                signAnimator.SetBool("ComboDone", true);
+                signAnimator.SetBool("New8", true);
+            }
+            else // if it's the first combo
+            {
+                // sign slides in 
+                signAnimator.SetBool("ComboDone", false);
+                signAnimator.SetBool("New8", false);
+                signAnimator.SetBool("NewCombo", true);
+            }
 
             // change sign colour
             ChangeColor();
-            Debug.Log("change colour pls");
         } 
+        // else if player cashed in or lost their combo
         else if (currentCombo < prevCombo)
         {
+            // sign slides out
             signAnimator.SetBool("NewCombo", false);
             signAnimator.SetBool("ComboDone", true);
+            signAnimator.SetBool("New8", false);
+        }
+        else
+        {
+            signAnimator.SetBool("ComboDone", false); // sign stays onscreen
         }
 
         prevCombo = currentCombo;
@@ -73,7 +69,7 @@ public class ComboSign : MonoBehaviour
         float hue = Random.Range(0f, 1f);
 
         // Convert hue to RGB and apply it to the sprite renderer
-        Color randomColor = Color.HSVToRGB(hue, 0.6f, 1f);
+        Color randomColor = Color.HSVToRGB(hue, 0.8f, 0.6f);
         signImage.color = randomColor;
     }
 }
